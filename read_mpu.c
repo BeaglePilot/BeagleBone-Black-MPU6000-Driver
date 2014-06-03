f0d56ec17437002fb6d483599c0c86a7dba3c188
Jimmy Johnson <catch22@fastmail.net>
2014-06-02 23:08:04 -0700

    Adding updated demo program

--- read_mpu.c
+++ read_mpu.c
@@ -5,42 +5,45 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <inttypes.h>
+#include <time.h>
 
 typedef struct MPUReport {
-	uint8_t		status;
-	int16_t		accel_x;
-	int16_t		accel_y;
-	int16_t		accel_z;
-	int16_t		temp;
-	int16_t		gyro_x;
-	int16_t		gyro_y;
-	int16_t		gyro_z;
-} MPUReport;
+	struct timeval timestamp;
+	unsigned long error_count;
+
+	int16_t gyro_x_raw;
+	int16_t gyro_y_raw;
+	int16_t gyro_z_raw;
+
+	int16_t accel_x_raw;
+	int16_t accel_y_raw;
+	int16_t accel_z_raw;
+	int16_t temperature_raw;
+}MPUReport;
 
 int main(int argc, char* argv[]) {
 
     int input_fd;
-    MPUReport buffer;      /* Character buffer */
+    MPUReport buffer;
 
-    /* Create input file descriptor */
-    input_fd = open ("/dev/mpu_6000_device", O_RDONLY);
+    input_fd = open ("/dev/mpu6000", O_RDONLY);
 
     if (input_fd == -1) {
             perror ("open");
             return 2;
     }
 
-    /* Copy process */
     while(1) {
     	read (input_fd, &buffer, sizeof(MPUReport));
 
-		printf("%d %d %d, %d, %d %d %d\n",
-				buffer.accel_x,
-				buffer.accel_y,
-				buffer.accel_z,
-				buffer.temp,
-				buffer.gyro_x,
-				buffer.gyro_y,
-				buffer.gyro_z);
+		printf("%10d %10d %10d | %10.2f | %10d %10d %10d | %ld.%ld\n",
+				buffer.accel_x_raw,
+				buffer.accel_y_raw,
+				buffer.accel_z_raw,
+				buffer.temperature_raw/340 + 36.53,
+				buffer.gyro_x_raw,
+				buffer.gyro_y_raw,
+				buffer.gyro_z_raw,
+				buffer.timestamp.tv_sec,buffer.timestamp.tv_usec);
     }
 }
